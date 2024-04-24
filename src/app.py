5dc74e9b165fbb2184571203c15d13fce961eaed
littlebutt <luogan199686@gmail.com>
2024-04-24 20:10:13 +0800

    feat: start a unix server

--- src/app.py
+++ src/app.py
@@ -1,9 +1,16 @@
+import time
+import os
+import hashlib
 from flask import Flask, render_template
+
+from unixv1 import UnixV1
 
 
 app = Flask(__name__)
 
 
 @app.route('/unix-v1')
-def index():
-    return render_template('unix-v1.html', hash="test")
+def unix_v1():
+    unix_server = UnixV1()
+    port, instance_hash = unix_server.start()
+    return render_template('unix-v1.html', hash=instance_hash, port=port)
---
+++ src/unixv1.py
@@ -0,0 +1,32 @@
+import random
+import time
+import hashlib
+import os
+import subprocess
+
+from typing import Tuple
+
+
+class UnixV1:
+
+    def start(self) -> Tuple[int, str]:
+        timestamp = time.time()
+        md5 = hashlib.md5()
+        md5.update(repr(timestamp).encode())
+        instance_hash = md5.hexdigest()
+        port = self.find_port()
+        p = subprocess.Popen(['tmux', 'new', '-s', instance_hash, '-d'])
+        p = subprocess.Popen(['tmux', 'send', '-t', instance_hash, 'ttyd', '-p', '8089', 'bash', 'Enter'], stdout=subprocess.DEVNULL)
+        time.sleep(1)
+        subprocess.Popen(f'tmux send -t {instance_hash} "cd /home/www & sh start.sh" Enter', shell=True)
+        print(f'port={port} instance_hash={instance_hash}')
+        return port, instance_hash
+
+    def find_port(self) -> int:
+        cmd = "netstat -ntl | grep -v Active | grep -v Proto | awk '{print $4}' | awk -F: '{print $NF}'"
+        procs = os.popen(cmd).read()
+        port = random.randint(1024, 49151)
+        if port in procs.split('\n'):
+            return self.find_port()
+        else:
+            return port
